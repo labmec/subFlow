e398b8963c19c0df8219a1b5fb12bbcd1207860c
giavancini <giovanea@unicamp.br>
2025-12-12 14:18:19 -0300

    feat: TransportAnalysis

---
+++ src/TSFTransportAnalysis.cpp
@@ -0,0 +1,228 @@
+//
+//  Created by Giovane Avancini on 04/11/25.
+//
+
+#include "TSFTransportAnalysis.h"
+#include "TPZVTKGenerator.h"
+
+using namespace std;
+
+TSFTransportAnalysis::TSFTransportAnalysis() : TPZLinearAnalysis() {}
+
+TSFTransportAnalysis::TSFTransportAnalysis(TPZCompMesh *cmesh, const RenumType &renumtype) : TPZLinearAnalysis(cmesh, renumtype) {}
+
+TSFTransportAnalysis::~TSFTransportAnalysis() {}
+
+void TSFTransportAnalysis::SetProblemData(TSFProblemData *simData) {
+  fSimData = simData;
+}
+
+TSFProblemData *TSFTransportAnalysis::GetProblemData() {
+  return fSimData;
+}
+
+int TSFTransportAnalysis::GetNumberOfIterations() {
+  return fKiteration;
+}
+
+void TSFTransportAnalysis::Initialize() {
+#ifdef PZ_USING_MKL
+  TPZSpStructMatrix<STATE> matrix(fCompMesh);
+#else
+  TPZSkylineNSymStructMatrix<STATE> matrix(fCompMesh);
+#endif
+  int n_threads = fSimData->fTNumerics.fNThreadsDarcy;
+  matrix.SetNumThreads(n_threads);
+  SetStructuralMatrix(matrix);
+  std::set<int> neumannMatids;
+  FillNeumannBCMatids(neumannMatids);
+  SetInitialSolution(neumannMatids);
+  ApplyEquationFilter(neumannMatids);
+  int nreducedeq = fStructMatrix->NReducedEquations();
+  TPZStepSolver<STATE> step;
+  step.SetDirect(ELDLt);
+  SetSolver(step);
+
+  std::cout << "Number of equations: " << fCompMesh->NEquations() << std::endl;
+  std::cout << "Number of elements: " << fCompMesh->NElements() << std::endl;
+}
+
+void TSFTransportAnalysis::RunTimeStep() {
+  TPZCompMesh *cmesh = Mesh();
+
+  int matIter = fSimData->fTNumerics.fMaxIterDarcy;
+  REAL res_norm = 1.0;
+  REAL corr_norm = 1.0;
+  REAL res_tol = fSimData->fTNumerics.fResTolDarcy;
+  REAL corr_tol = fSimData->fTNumerics.fCorrTolDarcy;
+  bool converged = false;
+
+  TPZFMatrix<STATE> sol = Solution();
+  for (fKiteration = 0; fKiteration < matIter; fKiteration++) {
+    Assemble();
+
+    // Check residual convergence
+    if (fKiteration > 0) {
+      TPZFMatrix<STATE> rhs = Rhs();
+      res_norm = Norm(rhs);
+      std::cout << "------Newton iteration: " << fKiteration << std::endl;
+      std::cout << "---------Residual norm: " << res_norm << std::endl;
+      std::cout << "---------Correction norm: " << corr_norm << std::endl;
+      if (res_norm < res_tol || corr_norm < corr_tol) {
+        std::cout << "------Iterative method converged with res_norm: " << res_norm << std::endl;
+        std::cout << "------Number of iterations = " << fKiteration << std::endl;
+        converged = true;
+        fSolution = sol;
+        break;
+      }
+    }
+    Solve();
+    TPZFMatrix<STATE> dsol = Solution();
+    corr_norm = Norm(dsol);
+    sol += dsol;
+    cmesh->LoadSolution(sol);
+    cmesh->TransferMultiphysicsSolution();
+  }
+
+  if (!converged) {
+    std::cout << "------Iterative method did not converge. res_norm: " << res_norm << " corr_norm: " << corr_norm << std::endl;
+  }
+}
+
+void TSFTransportAnalysis::PostProcessTimeStep(int dimToPost, int step) {
+  auto start_time_pp = std::chrono::steady_clock::now();
+
+  TPZStack<std::string, 10> scalnames, vecnames;
+
+  scalnames = fSimData->fTPostProcess.fScalnamesDarcy;
+  vecnames = fSimData->fTPostProcess.fVecnamesDarcy;
+
+  int div = 0;
+  if (dimToPost < 0) {
+    dimToPost = Mesh()->Reference()->Dimension();
+  }
+
+  std::string file = fSimData->fTPostProcess.fFileNameDarcy;
+  const int vtkRes = fSimData->fTPostProcess.fvtkResolution;
+
+  const std::string plotfile = file.substr(0, file.find(".")); // sem o .vtk no final
+  for (auto nm : vecnames) {
+    scalnames.Push(nm);
+  }
+
+  auto vtk = TPZVTKGenerator(fCompMesh, scalnames, plotfile, vtkRes, dimToPost);
+  vtk.SetStep(step);
+  int nthreads = fSimData->fTPostProcess.fNThreads;
+  vtk.SetNThreads(nthreads);
+  vtk.Do();
+
+  auto total_time_pp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time_pp).count() / 1000.;
+  cout << "Total time post process = " << total_time_pp << " seconds" << endl;
+}
+
+void TSFTransportAnalysis::Assemble() {
+  auto start_time_ass = std::chrono::steady_clock::now();
+
+  TPZLinearAnalysis::Assemble();
+
+  auto total_time_ass = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time_ass).count() / 1000.;
+  std::cout << "---------Time to assemble: " << total_time_ass << " seconds" << std::endl;
+}
+
+void TSFTransportAnalysis::Solve() {
+  auto start_time_solve = std::chrono::steady_clock::now();
+
+  TPZLinearAnalysis::Solve();
+
+  auto total_time_solve = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time_solve).count() / 1000.;
+  std::cout << "---------Time to solve: " << total_time_solve << " seconds" << std::endl;
+}
+
+void TSFTransportAnalysis::FillNeumannBCMatids(std::set<int> &neumannMatids) {
+  for (auto it = fSimData->fTBoundaryConditions.fBCDarcyMatIdToTypeValue.begin(); it != fSimData->fTBoundaryConditions.fBCDarcyMatIdToTypeValue.end(); ++it) {
+    int matid = it->first;
+    int bc_type = it->second.first;
+    if (bc_type == 1)
+      neumannMatids.insert(matid);
+  }
+}
+
+void TSFTransportAnalysis::SetInitialSolution(std::set<int> &neumannMatids) {
+  fCompMesh->LoadReferences();
+  TPZGeoMesh *gmesh = fCompMesh->Reference();
+
+  TPZFMatrix<STATE> &cmesh_sol = fCompMesh->Solution();
+  for (auto el : gmesh->ElementVec()) {
+    int elMatID = el->MaterialId();
+
+    if (neumannMatids.find(elMatID) == neumannMatids.end())
+      continue;
+
+    TPZCompEl *compEl = el->Reference();
+
+    int64_t nConnects = compEl->NConnects();
+
+    if (nConnects != 1)
+      DebugStop();
+
+    int64_t seq = compEl->Connect(0).SequenceNumber();
+    int ncorner = el->NCornerNodes();
+    REAL volume = el->Volume();
+
+    auto firstEq = fCompMesh->Block().Position(seq);
+
+    int64_t blockSize = fCompMesh->Block().Size(seq);
+
+    REAL val = fSimData->fTBoundaryConditions.fBCDarcyMatIdToTypeValue[elMatID].second;
+    val *= volume / ncorner;
+    for (int64_t eq = firstEq; eq < firstEq + blockSize; eq++) {
+      if (eq - firstEq < ncorner)
+        cmesh_sol.PutVal(eq, 0, val);
+    }
+  }
+
+  fCompMesh->TransferMultiphysicsSolution();
+
+  // When the internal dofs are condensed, the analysis solution size is different from the cmesh solution size
+  // Analysis only holds the independent equations, while cmesh holds all equations
+  // The independent equations are stored first in the cmesh solution vector, so we just need to copy them to the analysis solution
+  int cmesh_neq = fCompMesh->NEquations();
+  TPZFMatrix<STATE> &sol = Solution();
+  for (int i = 0; i < cmesh_neq; i++) {
+    sol.PutVal(i, 0, cmesh_sol.GetVal(i, 0));
+  }
+}
+
+void TSFTransportAnalysis::ApplyEquationFilter(std::set<int> &neumannMatids) {
+  fCompMesh->LoadReferences();
+  std::set<int64_t> removeEquations;
+  TPZGeoMesh *gmesh = fCompMesh->Reference();
+  TPZFMatrix<STATE> sol = fCompMesh->Solution();
+  for (auto el : gmesh->ElementVec()) {
+    int elMatID = el->MaterialId();
+
+    if (neumannMatids.find(elMatID) == neumannMatids.end()) continue;
+
+    TPZCompEl *compEl = el->Reference();
+
+    int64_t nConnects = compEl->NConnects();
+
+    if (nConnects != 1)
+      DebugStop();
+
+    int64_t seq = compEl->Connect(0).SequenceNumber();
+    int ncorner = el->NCornerNodes();
+
+    auto firstEq = fCompMesh->Block().Position(seq);
+
+    int64_t blockSize = fCompMesh->Block().Size(seq);
+
+    for (int64_t eq = firstEq; eq < firstEq + blockSize; eq++) {
+      removeEquations.insert(eq);
+    }
+  }
+
+  TPZEquationFilter filter(fCompMesh->NEquations());
+  filter.ExcludeEquations(removeEquations);
+  fStructMatrix->EquationFilter() = filter;
+}
---
+++ src/TSFTransportAnalysis.h
@@ -0,0 +1,93 @@
+//
+//  Created by Giovane Avancini on 04/11/25.
+//
+
+#pragma once
+
+#include "TPZLinearAnalysis.h"
+#include "TPZSkylineNSymStructMatrix.h"
+#include "TPZSpStructMatrix.h"
+#include "TPZYSMPMatrix.h"
+#include "TSFProblemData.h"
+#include "pzcmesh.h"
+#include "pzstepsolver.h"
+#include <stdio.h>
+
+class TSFTransportAnalysis : public TPZLinearAnalysis {
+
+public:
+  /// Data transfer object
+  TSFProblemData *fSimData;
+
+  /// Number of iterations
+  int fKiteration = 0;
+
+  bool fIsFirstAssemble = true;
+
+  TPZFYsmpMatrix<STATE> fMassMatrix;
+
+  TPZFYsmpMatrix<STATE> fTransmissibilityMatrix;
+
+  /// Default constructor
+  TSFTransportAnalysis();
+
+  /// Constructor based on a cmesh and optimization band directive
+  TSFTransportAnalysis(TPZCompMesh *cmesh, const RenumType &renumtype = RenumType::EDefault);
+
+  /// Default destructor
+  ~TSFTransportAnalysis();
+
+  /// Configurates iternal members
+  void Initialize();
+
+  /// Set data transfer object
+  void SetProblemData(TSFProblemData *simData);
+
+  /// Get data transfer object
+  TSFProblemData *GetProblemData();
+
+  /// Get the number of iterations
+  int GetNumberOfIterations();
+
+  /// Run a time step
+  void RunTimeStep();
+
+  /// Render a vtk file with requested variables for a time step
+  void PostProcessTimeStep(int dimToPost = -1, int step = -1);
+
+  /// Perform a Newton iteration
+  void NewtonIteration();
+
+  /// override assemble to have timers
+  void Assemble() override;
+
+  /// override solve to have timers
+  void Solve() override;
+
+  void AssembleMass();
+
+  void AssembleTransmissibility();
+
+  /// Fill the set of material ids related to Neumann BCs
+  /// This method is used to apply Neumann BCs without using BigNumbers
+  void FillNeumannBCMatids(std::set<int> &neumannMatids);
+
+  /// Set the initial solution based on the boundary conditions
+  /// This method is used to apply Neumann BCs without using BigNumbers
+  void SetInitialSolution(std::set<int> &neumannMatids);
+
+  /// Remove equations related to Neumann BCs from the system
+  /// This method is used to apply Neumann BCs without using BigNumbers
+  void ApplyEquationFilter(std::set<int> &neumannMatids);
+
+  /// Verifies if the sum of the fluxes over all faces of an element is zero
+  void VerifyElementFluxes();
+
+  /// Update the density and coefficients
+  void UpdateDensityAndCoefficients();
+
+  /// Update the last state variables (saturation and pressure)
+  void SetLastStateVariables();
+  void SetLastStateSaturation();
+  void SetLastStatePressure();
+};
